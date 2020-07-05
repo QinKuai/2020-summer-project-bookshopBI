948fe14e66dd1741080838341a05cf044241c95d
QinKuai <qinkuaipublic@163.com>
2020-07-05 14:15:10 +0800

    更新conversion中的错误-part3

--- com/conversion_rate_analysis/buildConversionInput.py
+++ com/conversion_rate_analysis/buildConversionInput.py
@@ -5,4 +5,4 @@ def extract_data(start, end):
     hql = "\"insert into table conversion_input partition (dt='" + start + "-" + end + "') " \
            "select url,uuid,sessionid,csvp from clickstream_log where dt>= " + start + " and dt<= " + end + "\""
     print(hql)
-    # HiveUtil.execute_shell(hql)
+    HiveUtil.execute_shell(hql)
